8826813be75d3931be9dfe4b393a793a13531c4e
elijah gaytan <elijahgaytan@utexas.edu>
2019-12-25 01:27:09 -0600

    finished v1

--- app.js
+++ app.js
@@ -2,22 +2,59 @@
 var express = require('express');
 var app = express();
 var request = require('request');
+var bodyParser = require("body-parser");
 
+
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+//array
+var campgrounds = 
+[
+    {name: "Brokeback creek", image: "https://cdn.pixabay.com/photo/2017/09/26/13/50/rv-2788677__340.jpg"},
+    {name: "The Drag", image: "https://cdn.pixabay.com/photo/2015/07/09/01/59/picnic-table-837221__340.jpg"},
+    {name: "Cucc Camper", image: "https://cdn.pixabay.com/photo/2016/04/28/15/49/airstream-1359135__340.jpg"},
+    {name: "Brokeback creek", image: "https://cdn.pixabay.com/photo/2017/09/26/13/50/rv-2788677__340.jpg"},
+    {name: "The Drag", image: "https://cdn.pixabay.com/photo/2015/07/09/01/59/picnic-table-837221__340.jpg"},
+    {name: "Cucc Camper", image: "https://cdn.pixabay.com/photo/2016/04/28/15/49/airstream-1359135__340.jpg"},
+    {name: "Brokeback creek", image: "https://cdn.pixabay.com/photo/2017/09/26/13/50/rv-2788677__340.jpg"},
+    {name: "The Drag", image: "https://cdn.pixabay.com/photo/2015/07/09/01/59/picnic-table-837221__340.jpg"},
+    {name: "Cucc Camper", image: "https://cdn.pixabay.com/photo/2016/04/28/15/49/airstream-1359135__340.jpg"}
+];
 
 app.get('/', function(req,res){
     res.render('landing');
 });
 
 app.get('/campgrounds', function(req, res){
-        var campgrounds = 
-        [
-            {name: "Brokeback creek", image: "https://pixabay.com/get/50e9d4474856b108f5d084609620367d1c3ed9e04e50744e73287bd7954ac0_340.jpg"},
-            {name: "The Drag", image: "https://pixabay.com/get/57e8d3444855a914f6da8c7dda793f7f1636dfe2564c704c72297cd4944fc25d_340.jpg"},
-            {name: "Cucc Camper", image: "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72297cd4944fc25d_340.png"}
-        ];
+        //first refers to what var is called on ejs page, second refers to what we pass in
+        res.render('campgrounds', {campgrounds: campgrounds}) 
+});
 
-        res.render('campgrounds', {campgrounds: campgrounds}) //first refers to what var is called on ejs page, second refers to what we pass in
+//follow REST - here you can create a new campground
+//NOTE: get req data from post - req.body, get req data from get - req.query
+
+app.post("/campgrounds", function(req, res){
+    //get data from form
+    var name = req.body.name;
+    var img = req.body.image;
+    
+    //create new campground object
+    var newcampground = {
+        name: name,
+        image: img
+    };
+    
+    //push it onto array
+    campgrounds.push(newcampground);
+    res.redirect('/campgrounds');
+});
+
+//show the form that sends data to post route
+app.get('/campgrounds/new', function(req, res){
+    res.render("new");
+
 });
 
 app.listen(3000, function(){
