4811d73057482967a46fd79dc4a394fa94c489fe
elijah gaytan <elijahgaytan@utexas.edu>
2020-01-02 18:13:56 -0600

    introduced data persistence

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
 //reqs
-var express = require('express');
-var app = express();
-var request = require('request');
-var bodyParser = require("body-parser");
+var express = require('express'),
+    app = express(),
+    request = require('request'),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose")
 
-
-
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-//array
+//schema setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+Campground.create(
+    {
+        name: "Brokeback Creek",
+        image: "https://cdn.pixabay.com/photo/2017/09/26/13/50/rv-2788677__340.jpg"
+
+    }, function(err, campground){
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log("Created a new campground!");
+            console.log(campground);
+        }
+    });
+
+
+/* array
 var campgrounds = 
 [
     {name: "Brokeback creek", image: "https://cdn.pixabay.com/photo/2017/09/26/13/50/rv-2788677__340.jpg"},
@@ -22,14 +46,23 @@ var campgrounds =
     {name: "The Drag", image: "https://cdn.pixabay.com/photo/2015/07/09/01/59/picnic-table-837221__340.jpg"},
     {name: "Cucc Camper", image: "https://cdn.pixabay.com/photo/2016/04/28/15/49/airstream-1359135__340.jpg"}
 ];
+*/
 
 app.get('/', function(req,res){
     res.render('landing');
 });
 
 app.get('/campgrounds', function(req, res){
-        //first refers to what var is called on ejs page, second refers to what we pass in
-        res.render('campgrounds', {campgrounds: campgrounds}) 
+       
+       Campground.find({}, function(err, allCampgrounds){
+            if(err){
+                console.log(err);
+            }
+            else{
+                res.render("campgrounds", {campgrounds: allCampgrounds})
+            }
+       });
+       // res.render('campgrounds', {campgrounds: campgrounds}) 
 });
 
 //follow REST - here you can create a new campground
@@ -46,9 +79,17 @@ app.post("/campgrounds", function(req, res){
         image: img
     };
     
-    //push it onto array
-    campgrounds.push(newcampground);
-    res.redirect('/campgrounds');
+    //save to db, redirect back to campgrounds page if successful
+    Campground.create(newcampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.redirect('/campgrounds');
+        }
+
+    });
+    
 });
 
 //show the form that sends data to post route
